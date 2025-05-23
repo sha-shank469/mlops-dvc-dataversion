10f6487545a6e179afb11289f07808ef37f979c7
Shashank.Mishra <Shashank.Mishra@isourse.com>
2025-05-23 16:06:30 +0530

    second version of data

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ data = {
 
 df = pd.DataFrame(data)
 
+# Adding new row to df for V2
+new_row_loc = {'Name':'Shashank', 'Age':20, 'city':'Delhi'}
+df.loc[len(df.index)] = new_row_loc
+
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 
